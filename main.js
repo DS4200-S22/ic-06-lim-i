92fc7ae01d2e9a671d8e1b40e2028b0c82299c80
lim-i <lim.i@northeastern.edu>
2022-02-10 13:22:09 -0500

    as far as I got

--- main.js
+++ main.js
@@ -7,8 +7,15 @@ Modified: 12/06/21
 */
 
 //Starter code for Button 1 click
-    // You will need to call this function when Button 1 is clicked
-    // You will need to modiffy the body of this function as described in the assignment Readme
+// You will need to call this function when Button 1 is clicked
+// You will need to modify the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+  let rand = Math.floor(Math.random() * 101)
+  let randomNum = document.getElementById("random_num")
+  randomNum.innerHTML = rand
+}
+
+function button2Clicked() {
+  let currentColor = document.getElementById("circleColor").style.background
+  currentColor.innerHTML = "green"
 }
